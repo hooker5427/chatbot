836e149f6a9f68c799710f02411a07e4b54a7dec
e <2756663963@qq.com>
2020-05-13 12:28:42 +0800

    execute_v2

--- exe_run.py
+++ exe_run.py
@@ -4,23 +4,25 @@ import sys
 import time
 import tensorflow as tf
 import seq2seqModel
-import io
+import jieba
+import pickle
+import warnings
 
+warnings.filterwarnings('ignore')
 
 max_length_inp, max_length_tar = 20, 20
 vocab_inp_size = 20000
 vocab_tar_size = 20000
-embedding_dim=128
+embedding_dim = 128
 units = 256
 max_train_data_size = 50000
 batch_size = 128
 
-
-
 from data_utils import read_data
+
+
 def preprocess_sentence(w):
-    w ='start '+ w + ' end'
-    #print(w)
+    w = 'start ' + w + ' end'
     return w
 
 
@@ -28,19 +30,15 @@ def max_length(tensor):
     return max(len(t) for t in tensor)
 
 
-input_tensor, input_index_word, input_word_index, input_vocab, target_tensor, target_index_word, target_word_index, target_vocab = read_data(
-    'new_corpus.txt', num_examples=None)
-
-
 def train():
     steps_per_epoch = len(input_tensor) // batch_size
     print(steps_per_epoch)
     enc_hidden = seq2seqModel.encoder.initialize_hidden_state()
-    # checkpoint_dir = gConfig['model_data']
-    # ckpt = tf.io.gfile.listdir(checkpoint_dir)
-    # if ckpt:
-    #     print("reload pretrained model")
-    #     seq2seqModel.checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    checkpoint_dir = 'models'
+    ckpt = tf.io.gfile.listdir(checkpoint_dir)
+    if ckpt:
+        print("reload pretrained model")
+        seq2seqModel.checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
     BUFFER_SIZE = len(input_tensor)
     dataset = tf.data.Dataset.from_tensor_slices((input_tensor, target_tensor)).shuffle(BUFFER_SIZE)
     dataset = dataset.batch(batch_size, drop_remainder=True)
@@ -61,7 +59,8 @@ def train():
         current_steps = +steps_per_epoch
         step_time_total = (time.time() - start_time) / current_steps
 
-        print('训练总步数: {} 每步耗时: {}  最新每步耗时: {} 最新每步loss {:.4f}'.format(current_steps, step_time_total, step_time_epoch,step_loss.numpy()))
+        print('训练总步数: {} 每步耗时: {}  最新每步耗时: {} 最新每步loss {:.4f}'.format(current_steps, step_time_total, step_time_epoch,
+                                                                      step_loss.numpy()))
         seq2seqModel.checkpoint.save(file_prefix=checkpoint_prefix)
 
         sys.stdout.flush()
@@ -71,7 +70,6 @@ def predict(sentence):
     checkpoint_dir = 'models'
     seq2seqModel.checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
     sentence = preprocess_sentence(sentence)
-    import  jieba
     inputs = [input_word_index.get(i, input_word_index['unk']) for i in jieba.lcut(sentence)]
     inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs], maxlen=max_length_inp, padding='post')
     inputs = tf.convert_to_tensor(inputs)
@@ -91,7 +89,7 @@ def predict(sentence):
 
         if target_index_word[predicted_id] == 'end':
             break
-        result += target_index_word[predicted_id] + ' '
+        result += target_index_word[predicted_id]
 
         dec_input = tf.expand_dims([predicted_id], 0)
 
@@ -99,4 +97,44 @@ def predict(sentence):
 
 
 if __name__ == '__main__':
-    train()
+    # train()
+    traing = False
+    if traing:
+        input_tensor, input_index_word, input_word_index, input_vocab, target_tensor, target_index_word, target_word_index, target_vocab = read_data(
+            'new_corpus.txt', num_examples=None)
+
+        meta = {
+            'input_tensor': input_tensor,
+            'input_index_word': input_index_word,
+            'input_word_index': input_word_index,
+            'input_vocab': input_vocab,
+            'target_tensor': target_tensor,
+            'target_index_word': target_index_word,
+            'target_word_index': target_word_index,
+            'target_vocab': target_vocab
+
+        }
+
+        with open('mypickle.pickle', 'wb') as f:
+            pickle.dump(meta, f)
+    else:
+        with open('mypickle.pickle', 'rb') as f:
+            meta = pickle.load(f)
+        input_tensor = meta['input_tensor']
+        input_index_word = meta['input_index_word']
+        input_word_index = meta['input_word_index']
+        input_vocab = meta['input_vocab']
+
+        target_tensor = meta['target_tensor']
+        target_index_word = meta['target_index_word']
+        target_word_index = meta['target_word_index']
+        target_vocab = meta['target_vocab']
+
+        print("开始对话：")
+        while True:
+            print(">", end='')
+            input_str = input()
+            ans_str = predict(input_str.strip())
+            print("<", ans_str)
+
+  
